47b767f2d4679eed97266a643b98671725b8c3b2
Houston Golden <houstongolden@gmail.com>
2013-11-20 02:54:23 -0800

    sumzy is born.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sumzy::Application.config.secret_key_base = '9401bc3f4775300208b6be1aaecdeb7a51b5c2ef1573bf4085a961aed81c728465f403815e875bfcc7b0ff53f7a246c36d673a42cc72998c02d25ad0f2ad5320'
