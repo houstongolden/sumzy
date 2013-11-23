ddd7162c6bb168e6ba8740fa8e1115a590263ab9
Roy Lee <royglee@gmail.com>
2013-11-23 12:02:39 -0800

    removed troubleshooting pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.from_omniauth(env["omniauth.auth"])
-    require 'pry';binding.pry
     session[:user_id] = user.id
     redirect_to root_url, notice: "Signed in!"
   end
