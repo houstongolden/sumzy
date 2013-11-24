21f5ce938c5d717dbba8da280e03531995ed83b4
Houston Golden <houstongolden@gmail.com>
2013-11-23 23:46:38 -0800

    smaller navbar icons and a broken profile photo

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "dhMrrXMekdrL3wKEbn4w", "ayc19GH5oyDkX94NQ4gnBvTdUGK9tkn2MF58nzc4cuU"
+  provider :twitter, "m11Uc0cRM7nyFfhEBP8w", "nhye8I2UUIp4mG1yR1kynBfZ4ztZGWNwYqxVSVpUA"
   provider :facebook, "1435406776682568", "348c6f652dfa71fabdd714d583c1f413", {:scope => 'read_stream,publish_stream,offline_access,email'}
 end
 
