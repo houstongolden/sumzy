8aa6c49b322fb21d3f1c4e5d9ab8a41cbbfe78a8
Roy Lee <royglee@gmail.com>
2013-11-23 19:38:31 -0800

    added facebook feed methods

--- lib/facebook.rb
+++ lib/facebook.rb
@@ -1,11 +1,28 @@
 module Facebook
 
   def get_basic_info
-    Koala::Facebook::API.new(self.facebook_token).get_object("me")
+    facebook_api_object.get_object("me")
   end
 
   def get_friends
-    Koala::Facebook::API.new(self.facebook_token).get_connections("me", "friends")
+    facebook_api_object.get_connections("me", "friends")
   end
 
+  def get_feed
+    #to = Time.now.to_i
+    #yest = 1.year.ago.to_i
+    facebook_api_object.fql_query("SELECT post_id, actor_id, target_id, message, attachment FROM stream WHERE filter_key in (SELECT filter_key FROM stream_filter WHERE uid=me() AND type='newsfeed') AND is_hidden = 0")
+    #facebook_api_object.fql_query("SELECT post_id, actor_id, target_id, message, likes FROM stream WHERE source_id = me() AND created_time > #{yest} AND created_time < #{to}")
+ 
+  end
+
+  def write_to_wall(message)
+    facebook_api_object.put_connections("me", "feed", :message => message)
+  end
+
+  private
+
+  def facebook_api_object
+    Koala::Facebook::API.new(self.facebook_token)  
+  end
 end
