88c24a2636a3d26dab7810f94b8e703fd4c36250
Roy Lee <royglee@gmail.com>
2013-12-16 21:47:50 -0800

    fixed twitter feed to work

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,7 +1,8 @@
 class FeedsController < ApplicationController
 
 	def index
-		@feeds = current_user.get_facebook_feed
+		@facebook_feed = current_user.get_facebook_feed if current_user.facebook_uid.present?
+    @twitter_feed = current_user.get_twitter_feed if current_user.twitter_uid.present? 
 	end
 	
 
