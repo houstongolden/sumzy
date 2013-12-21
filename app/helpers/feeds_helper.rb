05782d554150ede45937213e9a027ed60a18812d
Houston Golden <houstongolden@gmail.com>
2013-12-20 19:16:55 -0800

    rake db drop and push to heroku

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -3,17 +3,18 @@ module FeedsHelper
 
 		def linkify(text)
 		newtext = text.gsub(/\b((https?:\/\/|ftps?:\/\/|mailto:|www\.)([A-Za-z0-9\-_=%&@\?\.\/]+))\b/) {
+		#newtext = text.gsub(/\b((https?:\/\/|ftps?:\/\/|mailto:|@\w+|www\.)([A-Za-z0-9\-_=%&@\?\.\/]+))\b/) {
 		match = $1
 		tail  = $3
 			case match
 				when /^www/     then  "<a href=\"http://#{match}\">#{match}</a>"
 				when /^mailto/  then  "<a href=\"#{match}\">#{tail}</a>"
-				# when /^@/		then "<a href"\"http://twitter.com/#{match}\">#{match}</a>"
+				# when /^@/		then  "<a href=\"http://twitter.com/#{match}\">#{match}</a>"
 				else 
 				"<a href=\"#{match}\">#{match}</a>"
 			end
 		}
 		newtext
-	end	
+		end	
 
 end
