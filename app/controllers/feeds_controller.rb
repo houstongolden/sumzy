73fd67a31f9ac0b54384a0c243c2e41c22157235
Houston Golden <houstongolden@gmail.com>
2014-09-27 16:52:19 -0700

    sumzy is in pre beta

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -4,10 +4,6 @@ class FeedsController < ApplicationController
 		@facebook_feed = current_user.get_facebook_feed if current_user.facebook_uid.present?
   		@twitter_feed = current_user.get_twitter_feed if current_user.twitter_uid.present? 
 
-  		# @twitter_feed.map do |tweet|
-  		# 	linkify(tweet.to_s)
-  		# end
-
 	end
 	
 
